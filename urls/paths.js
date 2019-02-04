04bcb24ce3e6af108f82c56e992c8a0c94b628a8
Sudheer Mudumala <sudheer.mudumala.contractor@weather.com>
2019-02-04 13:50:33 -0500

    sf

--- urls/paths.js
+++ urls/paths.js
@@ -2,6 +2,7 @@ var pathConfig = {};
 
 pathConfig.array = ['/',
 '/weather/today/l/USDC0001:1:US',
+'/weather/today/l/USGA0028:1:US',
 '/weather/hourbyhour/l/USDC0001:1:US']
 
 module.exports = pathConfig;
