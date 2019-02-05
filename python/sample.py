3afaa1750c4ee92ec50339d691ff2b3ce007b041
Sudheer Mudumala <sudheer.mudumala.contractor@weather.com>
2019-02-05 13:15:37 -0500

    Added python script

--- python/sample.py
+++ python/sample.py
@@ -1,1 +1,23 @@
+import json
+
 print("hello world")
+
+f=open("../backstop_data/html_report/config.js","r")
+if f.mode=="r":
+	contents=f.read()
+#	print(contents)
+
+contents = contents.replace("report(","")
+contents = contents.replace(");","")
+
+#print(contents)
+
+contentsJson = json.loads(contents)
+
+#print(contentsJson["tests"])
+contentTests = contentsJson["tests"]
+
+for tests in contentTests:
+	if tests['status'] == 'fail':
+		failLabel = tests['pair']
+		print(failLabel['label'])
